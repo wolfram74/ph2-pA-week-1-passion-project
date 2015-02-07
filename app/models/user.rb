8e93899b1bca3132a38c91db1f53c8abc9432af9
Peter Haugen <phaugen@gmail.com>
2015-02-07 12:05:11 -0800

    setting up framework for testing

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+  has_many :urls
+  include BCrypt
+
+  # validates :password, presence: true, length: {mininum: 6}
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    # raise "Stuff borked"
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+end
---
+++ db/migrate/20150207110756_make_users.rb
@@ -0,0 +1,9 @@
+class MakeUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password_hash
+      t.timestamps
+    end
+  end
+end
